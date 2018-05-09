24180bf0e3463862f0243a7b540271a3e017c42c
Binaya Dahal <bidahal@deerwalk.com>
2018-05-09 17:47:25 +0545

    working with new design for new project

---
+++ design/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Home Page</title>
+        <link href="css/style.css" type="text/css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="page_wrap">
+            <div class="header">
+                <ul>
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                </ul>
+            </div>
+            <div class="content_wrap">
+                <div class="content">test1</div>
+                <div class="content">test2</div>
+            </div>
+        </div>
+    </body>
+</html>
